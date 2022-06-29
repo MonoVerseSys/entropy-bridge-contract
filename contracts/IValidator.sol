7b5179985517ffa5a5c55b64887fd5d9d85c36c1
hodong <hodong.kim@monoverse.io>
2022-06-29 15:16:50 +0900

    init bridge.sol

---
+++ contracts/IValidator.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+interface IValidator {
+
+    function addValidator(address validator)  external;
+    
+    function removeValidator(address validator)  external;
+
+    function getValidatorCount()  external view returns(uint256);
+
+    function isValiator(address user) external view returns(bool);
+
+}
---
+++ contracts/Validator.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+
+import "./openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
+import "./IValidator.sol";
+
+contract Validator is IValidator, Initializable, AccessControlEnumerableUpgradeable {
+
+    bytes32 public constant VALIDATOR_ROLE = keccak256("VALIDATOR_ROLE");
+    uint256 constant LIMIT = 30;
+
+    function initialize(address admin) public initializer {
+        require(admin != address(0), "zero address");
+        __AccessControlEnumerable_init();
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+    }
+
+    function addValidator(address validator) public override onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(validator != address(0), "zero address");
+        uint256 count = getValidatorCount();
+        require(count < LIMIT, "validator exceeded");
+        _grantRole(VALIDATOR_ROLE, validator);
+    }
+    
+    function removeValidator(address validator) public override onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(validator != address(0), "zero address");
+        _revokeRole(VALIDATOR_ROLE, validator);
+    }
+
+    function getValidatorCount() public view override returns(uint256) {
+        return getRoleMemberCount(VALIDATOR_ROLE);
+    }
+
+    function isValiator(address user) public view override returns(bool) {
+        return hasRole(VALIDATOR_ROLE, user);
+    }
+
+}
