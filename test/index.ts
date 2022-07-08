27dd9c99bb28e266a46194a7b64f03a8dfd6c2e3
hodong <hodong.kim@monoverse.io>
2022-07-08 19:07:34 +0900

    test code

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { ethers } from 'hardhat'
+import { ethers, upgrades } from 'hardhat'
 
 describe('Test', function () {
     it('Test ecdsa', async function () {
@@ -22,5 +22,71 @@ describe('Test', function () {
         console.log(`address: ${address}`)
         console.log(`result: `, result)
         expect(result[result.length - 1]).to.equal(address)
+    })
+
+    it('Confirm Test', async function () {
+        const Validator = await ethers.getContractFactory('Validator')
+        const signers = await ethers.getSigners()
+
+        const admin = signers[0].address
+        const validator = await upgrades.deployProxy(Validator, [admin])
+        const validatorAddress = (await validator.deployed()).address
+        console.log(validatorAddress)
+
+        expect(ethers.utils.isAddress(validatorAddress)).to.be.true
+
+        await validator.addValidator(signers[1].address)
+        await validator.addValidator(signers[2].address)
+        await validator.addValidator(signers[3].address)
+        await validator.addValidator(signers[4].address)
+
+        const validatorCnt = await validator.getValidatorCount()
+        console.log(validatorCnt.toString())
+        expect(validatorCnt.toString()).to.equal('4')
+
+        const NativeBridge = await ethers.getContractFactory('NativeBridge')
+        const nativeBridge = await upgrades.deployProxy(NativeBridge, ['test bridge', validatorAddress])
+        const nativeBridgeAddress = (await nativeBridge.deployed()).address
+
+        const depositer = signers[10]
+
+        const _nativeBridge = await nativeBridge.connect(depositer)
+        const sendValue = ethers.utils.parseUnits('0.001', 'ether')
+        const receipt = await _nativeBridge.depositNative(depositer.address, { value: sendValue })
+        const result = await receipt.wait()
+        console.log(JSON.stringify(result))
+        console.log(result.blockNumber)
+
+        const balance = await ethers.provider.getBalance(nativeBridgeAddress)
+        expect(balance.toString()).to.equal(sendValue)
+
+        const blockNumber = result.blockNumber
+        const hash = result.events[0].topics[1]
+        const trId = parseInt(ethers.utils.hexStripZeros(result.events[0].topics[2]))
+        const chainId = 31337
+        const owner = depositer.address
+
+        // console.log(hash, trId)
+
+        // const hash = result.events.topics[0]
+        // console.log(result.blockNumber)
+
+        // console.log(JSON.stringify(result.events[0]))
+
+        for (let i = 1; i <= 3; i++) {
+            const signature = await signers[i].signMessage(ethers.utils.arrayify(hash))
+            const receipt2 = await nativeBridge.confirmWithdrawal(blockNumber, trId, chainId, owner, sendValue, signature)
+            const result2 = await receipt2.wait()
+            console.log(i, ':', result2.events.length)
+        }
+        const balance2 = await ethers.provider.getBalance(nativeBridgeAddress)
+        expect(balance2.toString()).to.equal('0')
+
+        // const amount = await ethers.provider.getBalance(nativeBridgeAddress)
+        // console.log('amount', amount.toString())
+
+        // expect(amount.toString()).to.equal(sendValue)
+
+        // confirmWithdrawal()
     })
 })
